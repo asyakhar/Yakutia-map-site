b6df5d116a81f3caa9739e9fc04a540295519164
AsyaKhar <409792@edu.itmo.ru>
2026-05-09 23:14:21 +0300

    update fot Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // 👈 Включает экспорт статического HTML
+  images: {
+    unoptimized: true,  // GitHub Pages не поддерживает оптимизацию изображений Next.js
+  },
+  trailingSlash: true,  // Рекомендуется для GitHub Pages
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
